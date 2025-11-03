c6cb0bde7d5315c16ef7bf55533c4c337c73e5fa
Douwe van Loenen <douwe@advanceinsight.dev>
2025-11-03 20:21:40 +0100

    Edit button

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,16 +35,21 @@ html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 
 html_theme_options = {
+    "source_repository": "https://github.com/advanceinsight/agrios_documentation/",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
     "light_css_variables" : {"color-brand-primary" : "#002f87"}, "dark_css_variables" : {"color-brand-primary" : "#00a7e1"}
     }
 
-html_context = {
-    "display_github": True,                         # show the “Edit on GitHub” link
-    "github_user": "advanceinsight",                # ⬅️ your GitHub org/user
-    "github_repo": "agrios_documentation",          # ⬅️ your repo
-    "github_version": "main",                       # branch
-    "conf_py_path": "/docs/source/",                # path to your docs root, WITH trailing slash
-}
+
+
+# html_context = {
+#     "display_github": True,                         # show the “Edit on GitHub” link
+#     "github_user": "advanceinsight",                # ⬅️ your GitHub org/user
+#     "github_repo": "agrios_documentation",          # ⬅️ your repo
+#     "github_version": "main",                       # branch
+#     "conf_py_path": "/docs/source/",                # path to your docs root, WITH trailing slash
+# }
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
