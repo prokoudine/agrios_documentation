e9b1391d6cf1dfe5e9fe9f442b27da475514158d
Douwe van Loenen <douwe@advanceinsight.dev>
2025-11-03 19:51:53 +0100

    Revert back to theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'furo'
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 
