133bfdf629a41719311db3b045e9c74d18bbb9d9
Douwe van Loenen <douwe@advanceinsight.dev>
2025-11-03 20:11:25 +0100

    Revenrt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,10 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = ['_themes']
+# html_theme_path = ['_themes']
 # html_theme = 'furo'
-# html_static_path = ["_static"]
-# html_css_files = ["custom.css"]
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 html_context = {
     "display_github": True,                         # show the “Edit on GitHub” link
