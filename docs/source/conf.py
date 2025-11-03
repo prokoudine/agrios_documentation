5a41e9f8f47c57dc5c167957a6e1ad1af053323b
Douwe van Loenen <douwe@advanceinsight.dev>
2025-11-03 20:09:42 +0100

    Local theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,16 +31,17 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes']
 # html_theme = 'furo'
-html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+# html_static_path = ["_static"]
+# html_css_files = ["custom.css"]
 
 html_context = {
-    "display_github": True,          # show the “Edit on GitHub” link
-    "github_user": "advanceinsight", # ⬅️ your GitHub org/user
-    "github_repo": "AgriOS",         # ⬅️ your repo
-    "github_version": "main",        # branch
-    "conf_py_path": "/docs/source",        # path to your docs root, WITH trailing slash
+    "display_github": True,                         # show the “Edit on GitHub” link
+    "github_user": "advanceinsight",                # ⬅️ your GitHub org/user
+    "github_repo": "agrios_documentation",          # ⬅️ your repo
+    "github_version": "main",                       # branch
+    "conf_py_path": "/docs/source/",                # path to your docs root, WITH trailing slash
 }
 
 # -- Options for EPUB output
