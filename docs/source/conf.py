c7f717273df49a4221996d05b07af1c9b5e6cbb8
Douwe van Loenen <douwe@advanceinsight.dev>
2025-11-03 19:50:40 +0100

    Add custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,8 @@ templates_path = ['_templates']
 
 # html_theme = 'sphinx_rtd_theme'
 html_theme = 'furo'
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
